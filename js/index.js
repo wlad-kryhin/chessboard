1e814c6a1dcb03b79382c87004589c50ad7dcdd4
Wlad <wladkrychin@gmail.com>
2021-10-28 09:46:53 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
+let board = null;
+const game = new Chess();
+
+function onDragStart(source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false;
+
+  // only pick up pieces for White
+  if (piece.search(/^b/) !== -1) return false;
+}
+
+function makeRandomMove() {
+  const possibleMoves = game.moves();
+
+  // game over
+  if (possibleMoves.length === 0) return;
+
+  const randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  game.move(possibleMoves[randomIdx]);
+  board.position(game.position());
+}
+
+function onDrop(source, target) {
+  // see if the move is legal
+  const move = game.move({
+    from: source,
+    to: target,
+    promotion: "q", // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return "snapback";
+
+  // make random legal move for black
+  window.setTimeout(makeRandomMove, 250);
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd() {
+  board.position(game.position());
+}
+
 const config = {
+  draggable: true,
   position: {
     a3: "wP",
     b3: "wP",
@@ -21,8 +65,10 @@ const config = {
     d8: "bQ",
     e8: "bK",
   },
-  draggable: true,
-  onchange: onchange,
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
 };
-const board = Chessboard("myBoard", config);
-function onChange() {}
+board = Chessboard("myBoard", config);
+console.log(board);
+board.move();
